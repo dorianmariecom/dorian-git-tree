f910c909800b72feb9d40cd96f4674fd27138210
Dorian Marié <dorian@dorianmarie.fr>
2021-10-10 11:19:17 +0200

    prettier and description

--- lib/dorian/git/tree.rb
+++ lib/dorian/git/tree.rb
@@ -4,7 +4,7 @@ module Dorian
   module Git
     class Tree
       def self.run
-        list(ARGV.first || ".")
+        list(ARGV.first || '.')
       end
 
       private
@@ -12,17 +12,19 @@ module Dorian
       def self.list(dir, start: true)
         puts dir if start
 
-        `git ls-tree HEAD #{Shellwords.escape(dir)} --name-only`.split("\n").sort.each do |line|
-          split = line.split('/')
-          puts format(line)
-          list(line + '/', start: false) if File.directory?(line)
-        end
+        `git ls-tree HEAD #{Shellwords.escape(dir)} --name-only`.split("\n")
+          .sort
+          .each do |line|
+            split = line.split('/')
+            puts format(line)
+            list(line + '/', start: false) if File.directory?(line)
+          end
       end
 
       def self.format(file)
         split = file.split('/')
         return file unless split.size > 1
-        "│  " * (split.size - 2) + "└── " + split.last
+        '│  ' * (split.size - 2) + '└── ' + split.last
       end
     end
   end
